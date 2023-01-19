7084149a3b29295eb9f3e9036ca3ab838d15872e
DerWake <nikiton.3232@mail.ru>
2023-01-19 17:58:31 +0300

    Добавил рандомизацию чисел в таблице

--- Second/2/script.js
+++ Second/2/script.js
@@ -1,3 +1,24 @@
+function load(arr){
+	let k = 0;
+	for(let i = 0; i < 5; i++){
+		for(let j = 0; j < 5; j ++){
+			table.tBodies[0].children[i].cells[j].innerHTML = arr[k];
+			k++;
+		}
+	}
+}
+
+function random(){
+	let randomRows = [];
+	let k = 0;
+	while(k < 25){
+		randomRows[k] = Math.floor(Math.random() * 100).toString();
+		k++;
+	}
+	return randomRows;
+
+}
+
 function sort() {
 	let sortedRows = [];
 	let k = 0;
@@ -10,11 +31,12 @@ function sort() {
 	sortedRows.sort(function(a, b) {
   		return a - b;
 	});
-	k = 0;
-	for(let i = 0; i < 5; i++){
-		for(let j = 0; j < 5; j ++){
-			table.tBodies[0].children[i].cells[j].innerHTML = sortedRows[k];
-			k++;
-		}
-	}
+	load(sortedRows);
 }
+
+
+
+window.onload = (event) => {
+  load(random());
+
+};
